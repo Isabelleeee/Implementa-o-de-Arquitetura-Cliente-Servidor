26f47b5596f27ec40780a257e3a12ea8b2574521
Isabelle da Costa Lopes <isabellecostalopes@gmail.com>
2026-05-06 08:52:49 -0300

    corrigindo erro 404

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,15 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], # Libera geral para o exercício
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app = FastAPI()
 
